8d52930a58c1d5646b50f3b4c4f23fb31966a49a
ajay <be.el.ajay@gmail.com>
2018-09-03 21:40:23 +0530

    Adding scriptable object. Adding new wave.

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour {
+
+    [SerializeField] WaveConfig waveConfig;
+    [SerializeField] float moveSpeed = 2f;
+
+    private int waypointIndex = 0;
+    private List<Transform> waypoints;
+
+    // Use this for initialization
+    void Start ()
+    {
+        waypoints = waveConfig.GetAllWaypoints();
+        transform.position = waypoints[waypointIndex].transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPos = waypoints[waypointIndex].transform.position;
+            var moveThisFrame = moveSpeed * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, moveThisFrame);
+
+            if (transform.position == targetPos)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
