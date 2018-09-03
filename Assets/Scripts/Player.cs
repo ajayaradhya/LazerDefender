9764257b2d0cd8c2c347d3070c062872347de992
ajay <be.el.ajay@gmail.com>
2018-09-03 18:13:33 +0530

    Restricting the movement. Adding paddings.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
 
     [SerializeField] float moveSpeed = 10;
+    [SerializeField] float paddingX = 1f;
+    [SerializeField] float paddingY = 1f;
 
-	// Use this for initialization
-	void Start () {
-		
+
+    float xMin, xMax, yMin, yMax;
+
+    // Use this for initialization
+    void Start () {
+        SetUpMovementBoundaries();
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    private void SetUpMovementBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + paddingX;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - paddingX;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + paddingY;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - paddingY;
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
         MoveHorizontal();
         MoveVertical();
@@ -21,14 +38,14 @@ public class Player : MonoBehaviour {
     private void MoveVertical()
     {
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newYPos = transform.position.y + deltaY;
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(transform.position.x, newYPos);
     }
 
     private void MoveHorizontal()
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
         transform.position = new Vector2(newXPos, transform.position.y);
     }
 }
