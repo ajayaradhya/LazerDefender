1d864b8ad19de51270d7920fb0cc672fee805fa5
ajay <be.el.ajay@gmail.com>
2018-09-03 20:28:14 +0530

    Adding corotines. Continuous fire when space is held.

---
+++ Assets/Scripts/Lazer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lazer : MonoBehaviour {
+
+    [SerializeField] float lazerSpeed = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var newPosition = transform.position.y + lazerSpeed;
+
+        transform.position = new Vector2(transform.position.x, transform.position.y + lazerSpeed);
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,33 +6,49 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     [SerializeField] float moveSpeed = 10;
-    [SerializeField] float paddingX = 1f;
-    [SerializeField] float paddingY = 1f;
+    [SerializeField] float padding = 1f;
+    [SerializeField] float paddingTop = 3f;
+
+    [SerializeField] GameObject playerLazer;
+    [SerializeField] float periodOfContinuousFiring = 0.1f;
 
 
     float xMin, xMax, yMin, yMax;
+    Coroutine fireCoroutine;
 
     // Use this for initialization
     void Start () {
-        SetUpMovementBoundaries();
+        SetUpMoveBoundaries();
 	}
-
-    private void SetUpMovementBoundaries()
-    {
-        Camera gameCamera = Camera.main;
-
-        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + paddingX;
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - paddingX;
-
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + paddingY;
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - paddingY;
-    }
 
     // Update is called once per frame
     void Update ()
     {
         MoveHorizontal();
         MoveVertical();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if(Input.GetButtonDown("Fire1") )
+        {
+            fireCoroutine = StartCoroutine(FireContinuously());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(fireCoroutine);
+        }
+    }
+
+    IEnumerator FireContinuously()
+    {
+        while(true)
+        {
+            var lazer = Instantiate(playerLazer, transform.position, Quaternion.identity) as GameObject;
+            Destroy(lazer, 1);
+            yield return new WaitForSeconds(periodOfContinuousFiring);
+        }
     }
 
     private void MoveVertical()
@@ -47,5 +63,16 @@ public class Player : MonoBehaviour {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
         transform.position = new Vector2(newXPos, transform.position.y);
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - paddingTop;
     }
 }
